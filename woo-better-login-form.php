4e2578790702fc8c5af13df0b4a13cc6f195ecc3
Luiz "Bills <luizpbills@gmail.com>
2022-07-21 17:18:07 -0300

    Update woo-better-login-form.php

--- woo-better-login-form.php
+++ woo-better-login-form.php
@@ -5,13 +5,13 @@ Version: 1.0.0
 Description: Better login and register forms for WooCommerce stores
 Author: Luiz Bills
 Author URI: https://luizpb.com
-
-Text Domain: woo-better-login-form
-Domain Path: /languages
-
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: woo-better-login-form
+Domain Path: /languages
 */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 add_action( 'plugins_loaded', 'wooblf_plugin_init' );
 function wooblf_plugin_init () {
@@ -32,9 +32,9 @@ function wooblf_plugin_init () {
 function wooblf_admin_notice_dependencies () {
 	$class = 'notice notice-error';
 	list( $plugin_name ) = \get_file_data( __FILE__, [ 'Plugin Name' ] );
-	$woocommerce_link = '<a target="_blank" href="https://wordpress.org/plugins/woocommerce/">WooCommerce</a>';
+	$woocommerce_link = esc_url( \admin_url( 'plugin-install.php?tab=plugin-information&plugin=woocommerce' ) );
 	$message = esc_html__( '%s requires %s installed and activated.', 'woo-better-login-form' );
-	$message = sprintf( $message, '<strong>' . esc_html( $plugin_name ) . '</strong>', $woocommerce_link );
+	$message = sprintf( $message, '<strong>' . esc_html( $plugin_name ) . '</strong>', "<a target=\"_blank\" href=\"$woocommerce_link\">WooCommerce</a>" );
 	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), $message );
 }
 
@@ -56,18 +56,30 @@ function wooblf_enqueue_scripts () {
 
 function wooblf_add_register_button () {
 	$text = esc_html__( "Don't have an account?", 'woo-better-login-form' );
-	$link = esc_html__( 'Register now', 'woo-better-login-form' );
+	$label = esc_html__( 'Register now', 'woo-better-login-form' );
+	$url = esc_url( wooblf_get_register_url() );
 	?>
-	<p id="register-link"><span><?= $text ?></span> <a href="#register"><?= $link ?></a></p>
+	<p id="register-link"><span><?= $text ?></span> <a href="<?= $url ?>"><?= $label ?></a></p>
 	<?php
 }
 
 function wooblf_add_login_button () {
 	$text = esc_html__( "Already have an account?", 'woo-better-login-form' );
-	$link = esc_html__( 'Sign in', 'woo-better-login-form' );
+	$label = esc_html__( 'Sign in', 'woo-better-login-form' );
+	$url = esc_url( wooblf_get_login_url() );
 	?>
-	<p id="login-link"><span><?= $text ?></span> <a href="#login"><?= $link ?></a></p>
+	<p id="login-link"><span><?= $text ?></span> <a href="<?= $url ?>"><?= $label ?></a></p>
 	<?php
+}
+
+function wooblf_get_login_url () {
+	$url = '#login';
+	return $url; 
+}
+
+function wooblf_get_register_url () {
+	$url = '#register';
+	return $url; 
 }
 
 function wooblf_body_class ( $classes ) {
